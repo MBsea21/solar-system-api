25e3cb33aaa402fcee12271f04dbefa12ee2c09e
Madeline Bennett <madeline.bennett.pm@gmail.com>
2024-10-21 20:59:38 -0700

    updated to include questions on whether plantet bp should be in its own file or not

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from .routes.routes import planet_bp
-
+#if using planets_path file for planets_bp it would look like the following
+#from .routes.planets_path import planet_bp
 def create_app(test_config=None):
     app = Flask(__name__)
     app.register_blueprint(planet_bp)
     return app
-

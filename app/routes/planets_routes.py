5efbb6d8363936b46ab22498788c4c7ce956842c
Madeline Bennett <madeline.bennett.pm@gmail.com>
2024-10-29 21:06:38 -0700

    wave 3 complete- get one book, delete, and update (put) functionality added

--- app/routes/planets_routes.py
+++ app/routes/planets_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort, make_response, request
+from flask import Blueprint, abort, make_response, request, Response
 from ..models.planet import Planet
 from ..db import db
 
@@ -32,3 +32,57 @@ def get_planets():
 
     planets_response = [planet.to_dict() for planet in planets]
     return planets_response
+
+@planet_bp.get("/<planet_id>")
+def get_one_planet(planet_id):
+    planet = validate_planet(planet_id)
+    response = planet.to_dict()
+    # response = {
+    #     "id": planet.id,
+    #     "name": planet.name,
+    #     "description": planet.description,
+    #     "signs of life": planet.signs_of_life 
+    # }
+    return response
+
+
+
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except: 
+        response = ({"message": f"planet {planet_id} invalid"},400)
+        abort(make_response(response))
+    
+    query = db.select(Planet).where(Planet.id == planet_id)
+    planet = db.session.scalar(query)
+
+    if not planet:
+        response = ({"message" : f"planet {planet_id} does not exist"}, 404)
+        abort(make_response(response))
+
+    return planet
+
+@planet_bp.put("/<planet_id>")
+def update_planet(planet_id):
+    planet = validate_planet(planet_id)
+    request_body = request.get_json()
+
+    planet.name = request_body["name"]
+    planet.description = request_body["description"]
+    planet.signs_of_life = request_body["signs of life"]
+
+    db.session.commit()
+    return Response(status=204, mimetype="application/json")
+
+
+@planet_bp.delete("/<planet_id>")
+def delete_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    db.session.delete(planet)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
